3be9ac5f2d9223420f8a66742fc77f621a23c545
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 16:03:34 +0545

    fix(Job):deleted and recreated to fix a bug

--- Job/models.py
+++ Job/models.py
@@ -1,18 +1,28 @@
 from django.db import models
+
+# Create your models here.
+from django.db import models
 from django.contrib.auth import get_user_model
 
 from User.models import SystemUser
 
+User = get_user_model()
+
 
 class Jobs(models.Model):
     title = models.CharField(max_length=150, blank=False, null=False)
-    team_lead = models.OneToOneField(SystemUser, on_delete=models.SET_NULL)
+    team_lead = models.ManyToManyField(SystemUser, null=True)
     required_position = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.title
 
 
 class Application(models.Model):
-    User = get_user_model()
     job = models.OneToOneField(Jobs, on_delete=models.SET_NULL, null=True, blank=False)
-    applicants = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="applicant")
+    applicant = models.ManyToManyField(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="applicant")
     team_lead = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="team_lead")
     cv = models.FileField(upload_to="\media\cv", null=False, blank=False)
+
+    def __str__(self):
+        return self.job.title
