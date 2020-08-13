f4103efb1f84fe046c0eee015a09bc88f876cbc6
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 12:24:55 +0545

    fix(Account):typo fixed

--- Account/urls.py
+++ Account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-app_name = 'user'
 
 from .views import UserLogoutView, UserSignupView, UserLoginView
 
-url_patterns = [
+app_name = 'user'
+
+urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('register/', UserSignupView.as_view(), name="register"),
     path('logout/', UserLogoutView.as_view(), name="logout"),
