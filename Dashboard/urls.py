6e8c71473f0a55ef97d04120b11fcf12fff80720
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 14:53:40 +0545

    fix(Dashboard):dashboard bug fixed

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import index
 
+app_name = "dashboard"
 urlpatterns = [
     path('index/', index, name="index"),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 def index(request):
+    # current_user = User.objects.get(id=request.)
+    # print(current_user.position)
+    print(request.user.user_position)
     return render(request, 'Dashboard/i-dashboard.html', context=None)
