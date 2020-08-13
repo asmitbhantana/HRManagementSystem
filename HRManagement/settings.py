b8b603531fa7c34aa1f75a21c232330ec3c4ca2a
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 15:09:28 +0545

    feat(Jobs,Application):created jobs and application

--- HRManagement/settings.py
+++ HRManagement/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'User',
     'Account',
     'Dashboard',
+    'Jobs',
 ]
 
 MIDDLEWARE = [
--- Jobs/models.py
+++ Jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from User.models import SystemUser
+
+
+class Jobs(models):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    team_lead = models.OneToOneField(SystemUser, on_delete=models.SET_NULL)
+    required_position = models.CharField(max_length=100, blank=False, null=False)
+
+
+class Application(models):
+    job = models.OneToOneField(Jobs, on_delete=models.SET_NULL, null=True, blank=False)
+    applicants = models.OneToOneField(Jobs, on_delete=models.SET_NULL, null=True, blank=False)
+    cv = models.FileField(upload_to="\media\cv", null=False, blank=False)
