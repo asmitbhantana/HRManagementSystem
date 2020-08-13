d3e29d1607309c948c9b18a67a3107f6471c8334
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 18:06:22 +0545

    fix(Job):manytomany changed to foregin key

--- Job/models.py
+++ Job/models.py
@@ -11,7 +11,7 @@ User = get_user_model()
 
 class Jobs(models.Model):
     title = models.CharField(max_length=150, blank=False, null=False)
-    team_lead = models.ManyToManyField(SystemUser, null=True)
+    team_lead = models.ForeignKey(SystemUser, on_delete=models.SET_DEFAULT, default=1, blank=False)
     required_position = models.CharField(max_length=100, blank=False, null=False)
 
     def __str__(self):
@@ -19,9 +19,10 @@ class Jobs(models.Model):
 
 
 class Application(models.Model):
-    job = models.OneToOneField(Jobs, on_delete=models.SET_NULL, null=True, blank=False)
-    applicant = models.ManyToManyField(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="applicant")
-    team_lead = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="team_lead")
+    job = models.OneToOneField(Jobs, on_delete=models.SET_DEFAULT, null=True, blank=False, default=0)
+    applicant = models.ForeignKey(User, on_delete=models.SET_DEFAULT, null=False, blank=False, related_name="applicant", default=1)
+    team_lead = models.OneToOneField(User, on_delete=models.SET_DEFAULT, null=False, blank=False,
+                                     related_name="team_lead", default=0)
     cv = models.FileField(upload_to="\media\cv", null=False, blank=False)
 
     def __str__(self):
