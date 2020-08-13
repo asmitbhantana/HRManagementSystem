eee7c68f6c22eac459703069592d4ef603b44d83
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 12:19:05 +0545

    feat(accounts):added url routing to the Account

--- Account/urls.py
+++ Account/urls.py
@@ -1,5 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
+app_name = 'user'
+
+from .views import UserLogoutView, UserSignupView, UserLoginView
 
 url_patterns = [
-    # url('',)
+    path('login/', UserLoginView.as_view(), name="login"),
+    path('register/', UserSignupView.as_view(), name="register"),
+    path('logout/', UserLogoutView.as_view(), name="logout"),
 ]
