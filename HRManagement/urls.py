d8eb96b30a58f02a7bc83eaf8ec2c60833d888c9
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 14:56:02 +0545

    fix(all):some fixed

--- HRManagement/urls.py
+++ HRManagement/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('Account.urls'), name='user'),
+    path('dashboard/', include('Dashboard.urls'), name="dashboard")
 
 ]
