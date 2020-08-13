ba1010054a07f31359d5be15f06573e9f3645ece
asmitbhantana <bhantanasmit@gmail.com>
2020-08-13 12:13:24 +0545

    fix(User):fixed position attribute on SystemUser

--- User/models.py
+++ User/models.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class SystemUser(AbstractUser):
+    hiring_manager = 'HR'
+    interviewee = 'I'
+    team_lead = 'TL'
     position = [
-        ('HR', 'HR'),
-        ('I', 'Interviewee'),
-        ('TL', 'Team Lead'),
+        (hiring_manager, 'Human Resource'),
+        (interviewee, 'Interviewee'),
+        (team_lead, 'Team Lead'),
     ]
+    user_position = models.CharField(
+        max_length=2,
+        choices=position,
+        default=interviewee,
+    )
