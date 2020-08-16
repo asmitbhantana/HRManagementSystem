894d0316629ff5aef91e3271ba0549774d223a6d
asmitbhantana <bhantanasmit@gmail.com>
2020-08-16 10:20:48 +0545

    feat(Exam):exam feature added

---
+++ Exam/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import FormView
+
+
+# Create your views here.
+class CreateQuestion(FormView):
+    pass
+
+
+class GiveExam(FormView):
+    pass
